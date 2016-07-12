5412032214530935e0a354990063fd39cfdfcb40
denismaster <denismaster@outlook.com>
2016-07-12 14:25:06 +0300

    Initial Commit

---
+++ TestTask2.Models/AuthorChangeAttribute.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestTask2.Models
+{
+    ///
+    /// <summary>
+    /// Атрибут, которым помечаются изменения исходного кода.
+    /// </summary>
+    ///
+    //Возможность использования нескольких атрибутов сразу
+    [System.AttributeUsage(System.AttributeTargets.Class |
+                       System.AttributeTargets.Struct,
+                       AllowMultiple = true) ]
+    public class AuthorChangeAttribute
+    {
+        private String authorName;
+        private DateTime date;
+        private String description;
+
+        public AuthorChangeAttribute(String authorName, DateTime date, String description=null)
+        {
+            if(String.IsNullOrEmpty(authorName))
+                throw new ArgumentException("Author name is invalid or null");
+
+            this.authorName = authorName;
+            this.date = date;
+            this.description = description;
+        }
+
+        public String AuthorName
+        {
+            get 
+            { 
+                return authorName;
+            }
+        }
+
+        public String Description
+        {
+            get
+            { 
+                return description;
+            }
+        }
+
+        public DateTime Date
+        {
+            get
+            {
+                return date;
+            }
+        }
+    }
+}
